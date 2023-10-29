c85b22ed218c6fd0c2282b90709cf49280aeff51
ICe1BotMaker <ice1github@gmail.com>
2023-10-29 19:34:10 +0900

    [Test] Modified to test the ability to transfer widgets and change properties

--- tests/cli.js
+++ tests/cli.js
@@ -2,13 +2,18 @@ const { CLIApplication, CLIPanel } = require(`../`);
 
 const app = new CLIApplication();
 
+const background = new CLIPanel({"paths": ["/root/background"]});
 const panel1 = new CLIPanel({
     "paths": ["/root/panel1"],
     "styles": {
-        "width": 1,
-        "height": 2,
+        "x": 3,
+        "y": 2,
+        "width": 10,
+        "height": 5,
+        "background-color": "#ff0000",
     }
 });
-app.append(panel1);
 
-app.do(1);
+app.append(background, panel1);
+
+app.show(30);
