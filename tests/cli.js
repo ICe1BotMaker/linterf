c5f52f97767b63f793497d4d7f460671500210ab
ICe1BotMaker <ice1github@gmail.com>
2023-10-31 21:00:04 +0900

    [Test] Code has been written to test if buttons can be drawn

--- tests/cli.js
+++ tests/cli.js
@@ -1,20 +1,27 @@
-const { CLIApplication, CLIPanel, CLILabel } = require(`../`);
+const { CLIApplication, CLIPanel, CLILabel, CLIButton } = require(`../`);
 
 const app = new CLIApplication();
 
-const background = new CLIPanel({"paths": ["/root/background"]});
-const panel1 = new CLIPanel({
-    "paths": ["/root/panel1"],
+const background = new CLIPanel({"paths": ["/root/background"], "styles": {"background-color": "#1F1717"}});
+const panel1 = new CLIPanel({"paths": ["/root/panel1"], "styles": {"x": 3, "y": 2, "width": 16, "height": 2, "background-color": "#CE5A67"}});
+const label1 = new CLILabel({"text": "lorem ipsum", "styles":{"x": 5, "y": 3}});
+const button1 = new CLIButton({
+    "text": " exit",
     "styles": {
+        "width": 16,
+        "height": 0,
         "x": 3,
-        "y": 2,
-        "width": 15,
-        "height": 5,
-        "background-color": "#ff0000",
+        "y": 6,
+        "background-color": "#F4BF96"
+    },
+    "events": {
+        "onclick": () => {
+            process.stdout.write(`\x1b[${process.stdout.rows - 1};${process.stdout.columns}H`);
+            process.exit();
+        }
     }
 });
-const label1 = new CLILabel({"paths": ["/root/label1"]});
 
-app.append(background, panel1, label1);
+app.append(background, panel1, label1, button1);
 
 app.show(30);
