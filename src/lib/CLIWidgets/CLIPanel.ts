6983864247c889a69e8a6640b99354a3c40b7381
ICe1BotMaker <ice1github@gmail.com>
2023-11-08 20:31:28 +0900

    [Improve] Function added as code moved

--- src/lib/CLIWidgets/CLIPanel.ts
+++ src/lib/CLIWidgets/CLIPanel.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import * as app from "../CLIApplication";
 
 export class CLIPanel {
@@ -22,5 +23,19 @@ export class CLIPanel {
 
     public constructor(props?: app.Iproperties) {
         if (props) this.data.properties = app.setProps(props, this.data.properties);
+    }
+
+    public prerun(widget: app.Iwidget, focus: string) {
+        const { styles } = widget.data.properties;
+
+        if (!styles.fill) styles.fill = `█`;
+
+        process.stdout.write(`\x1b[${styles.y};${styles.x}H`);
+        if (styles.width) console.log(focus + chalk.hex(styles[`background-color`] ? styles[`background-color`] : `#ffffff`)(styles.fill.repeat(styles.width)));
+        
+        if (styles.height) for (let i = 0; i < styles.height; i++) {
+            process.stdout.write(`\x1b[${styles.y + i};${styles.x}H`);
+            if (styles.width) console.log(focus + chalk.hex(styles[`background-color`] ? styles[`background-color`] : `#ffffff`)(styles.fill.repeat(styles.width)));
+        }
     }
 }
