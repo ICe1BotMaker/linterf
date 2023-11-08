d5acbb8040e8c312e98babbdd49ecbd35bfe098b
ICe1BotMaker <ice1github@gmail.com>
2023-11-08 20:31:16 +0900

    [Improve] Function added as code moved

--- src/lib/CLIWidgets/CLIButton.ts
+++ src/lib/CLIWidgets/CLIButton.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import * as app from "../CLIApplication";
 
 export class CLIButton {
@@ -8,8 +9,8 @@ export class CLIButton {
             "accepts": ["paths", "styles", "events", "text"],
             "paths": [],
             "styles": {
-                "x": 4,
-                "y": 3,
+                "x": 1,
+                "y": 1,
                 "text-color": "#ffffff",
                 "visible": true
             },
@@ -19,5 +20,22 @@ export class CLIButton {
 
     public constructor(props?: app.Iproperties) {
         if (props) this.data.properties = app.setProps(props, this.data.properties);
+    }
+
+    public prerun(widget: app.Iwidget, focus: string) {
+        const { styles, text } = widget.data.properties;
+
+        if (!styles.fill) styles.fill = `█`;
+
+        process.stdout.write(`\x1b[${styles.y};${styles.x}H`);
+        if (styles.width) console.log(focus + chalk.hex(styles[`background-color`] ? styles[`background-color`] : `#ffffff`)(styles.fill.repeat(styles.width)));
+        
+        if (styles.height) for (let i = 0; i < styles.height; i++) {
+            process.stdout.write(`\x1b[${styles.y + i};${styles.x}H`);
+            if (styles.width) console.log(focus + chalk.hex(styles[`background-color`] ? styles[`background-color`] : `#ffffff`)(styles.fill.repeat(styles.width)));
+        }
+
+        process.stdout.write(`\x1b[${styles.y};${styles.x}H`);
+        console.log(focus + chalk.bgHex(styles?.[`background-color`] ? styles[`background-color`] : `#000000`).hex(styles?.[`text-color`] ? styles[`text-color`] : `#ffffff`)(text));
     }
 }
