05ee26b4158dcdbca131210e670d7d03af23d219
ICe1BotMaker <ice1github@gmail.com>
2023-11-08 20:21:03 +0900

    [Feat] Check boxes are now available

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { CLIApplication } from "./lib/CLIApplication";
 export { CLIPanel } from "./lib/CLIWidgets/CLIPanel";
 export { CLILabel } from "./lib/CLIWidgets/CLILabel";
 export { CLIButton } from "./lib/CLIWidgets/CLIButton";
+export { CLICheckbox } from "./lib/CLIWidgets/CLICheckbox";
