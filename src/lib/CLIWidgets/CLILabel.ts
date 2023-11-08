51324be9dd285a6e6161980d22fcb371dedb45c9
ICe1BotMaker <ice1github@gmail.com>
2023-11-08 20:31:22 +0900

    [Improve] Function added as code moved

--- src/lib/CLIWidgets/CLILabel.ts
+++ src/lib/CLIWidgets/CLILabel.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import * as app from "../CLIApplication";
 
 export class CLILabel {
@@ -8,8 +9,8 @@ export class CLILabel {
             "accepts": ["paths", "styles", "text"],
             "paths": [],
             "styles": {
-                "x": 4,
-                "y": 3,
+                "x": 1,
+                "y": 1,
                 "text-color": "#ffffff",
                 "visible": true
             },
@@ -19,5 +20,32 @@ export class CLILabel {
 
     public constructor(props?: app.Iproperties) {
         if (props) this.data.properties = app.setProps(props, this.data.properties);
+    }
+
+    public prerun(widgets: Array<app.Iwidget>, widget: app.Iwidget, func: Function, focus: string) {
+        const { styles, text } = widget.data.properties;
+
+        let bwidget: boolean = false;
+        widgets.forEach((_widget: app.Iwidget) => {
+            if (func(_widget, styles)) {
+                bwidget = true;
+                
+                process.stdout.write(`\x1b[${styles.y};${styles.x}H`);
+
+                const backgroundColor = _widget.data.properties.styles["background-color"] || `#000000`;
+                const textColor = styles["text-color"] || _widget.data.properties.styles["text-color"] || `#ffffff`;
+
+                console.log(focus + chalk.bgHex(backgroundColor)(chalk.hex(textColor)(text)));
+            }
+        });
+
+        if (!bwidget) {
+            process.stdout.write(`\x1b[${styles.y};${styles.x}H`);
+
+            const backgroundColor = `#000000`;
+            const textColor = styles["text-color"] || `#ffffff`;
+
+            console.log(focus + chalk.bgHex(backgroundColor)(chalk.hex(textColor)(text)));
+        }
     }
 }
